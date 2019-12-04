e5622046729b9fd09d5cb59862bddd2284d96552
xanderstevenson <sentheard@4261811f904c>
2019-12-04 23:43:16 +0000

    fixed the urls in the posts app

--- posts/urls.py
+++ posts/urls.py
@@ -5,13 +5,14 @@ from django.views.generic import TemplateView
 from . import views
 from .views import CreatePhotoView
 
+
 urlpatterns = [
 
-    path('post_stuff/add_photos/', CreatePhotoView.as_view(), name='add_photos'),
-    path('post_stuff/add_audio/', views.add_audio, name="add_audio"),
-    path('post_stuff/add_video/', views.add_video, name="add_video"),
-    path('post_stuff/add_text/', views.add_text, name="add_text"),
-    path('post_stuff/', views.post_stuff, name="post_stuff"),
+    path('add_photos/', CreatePhotoView.as_view(), name='add_photos'),
+    path('add_audio/', views.add_audio, name="add_audio"),
+    path('add_video/', views.add_video, name="add_video"),
+    path('add_text/', views.add_text, name="add_text"),
+    path('', views.post_stuff, name="post_stuff"),
 
 ]
     # path('post_stuff/add_photos/', views.add_photos, name="add_photos"),
