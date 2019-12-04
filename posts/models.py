370e3c569f04494a4656bced09978041cc274b01
xanderstevenson <sentheard@4261811f904c>
2019-12-04 05:45:48 +0000

    used font awesome to add some cool icons for photos, video, Audio and text

--- posts/models.py
+++ posts/models.py
@@ -5,26 +5,29 @@ import uuid
 storage = S3Storage(aws_s3_bucket_name='django-static-sentheard')
 
 
-class Car(models.Model):
-    name = models.CharField(max_length=255)
-    photo = models.ImageField(storage=storage)
-
-
-
-class Post(models.Model):
-    title = models.TextField()
-    cover = models.ImageField(upload_to='images/')
-
-    def __str__(self):
-        return self.title
-
 class Photo(models.Model):
     uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False,
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100)
-    photo = models.FileField()
-
+    primary_key=True, default=uuid.uuid4, editable=False,)
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(storage=storage)
     def __str__(self):
         return self.title
+
+
+# class Post(models.Model):
+#     title = models.TextField()
+#     cover = models.ImageField(upload_to='images/')
+
+#     def __str__(self):
+#         return self.title
+
+# class Photo(models.Model):
+#     uuid = models.UUIDField(
+#         primary_key=True, default=uuid.uuid4, editable=False,
+#     )
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     title = models.CharField(max_length=100)
+#     photo = models.FileField()
+
+#     def __str__(self):
+#         return self.title
