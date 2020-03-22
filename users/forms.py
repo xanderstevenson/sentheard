1049bf9675c9ba11b887a61d8ba80bd181ee78f8
xanderstevenson <sentheard@8e86bfb0e784>
2020-03-22 04:11:30 +0000

    Custome User is coming along!"

--- users/forms.py
+++ users/forms.py
@@ -6,10 +6,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ('email',)
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ('email',)
--- users/models.py
+++ users/models.py
@@ -1,9 +1,21 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+# class CustomUserManager(UserManager):
+#     pass
+
 
 class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
+    username = None
+    email = models.EmailField(_('email'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
