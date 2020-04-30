f05a5dcf35fecfcf725dcf203688705a7aff418b
xanderstevenson <sentheard@8e86bfb0e784>
2020-04-30 17:08:02 +0000

    excluded is_staff and is_actuve from forms, and thus from the sign-up page

--- users/forms.py
+++ users/forms.py
@@ -6,9 +6,11 @@ class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+        exclude = ['is_staff', 'is_active']
+        fields = ('email', 'first_name', 'last_name')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+        exclude = ['is_staff', 'is_active']
+        fields = ('email', 'first_name', 'last_name')
